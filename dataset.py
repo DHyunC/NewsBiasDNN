134e4c4a0bfbfa545e1554e7efda196d2a66b73d
DHyunC <chsdaniel725@gmail.com>
2024-07-01 23:47:52 +1000

    starter code for dataloader generation

--- dataset.py
+++ dataset.py
@@ -5,5 +5,16 @@ Author: Daniel H Choi
 """
 
 from params import *
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
+def generate_train_dataloader():
+
+    return DataLoader()
+
+def generate_validation_dataloader():
+
+    return DataLoader()
+
+def generate_test_dataloader():
+
+    return DataLoader()
--- params.py
+++ params.py
@@ -10,3 +10,6 @@ betas = (0.9, 0.999)
 amsgrad = False
 
 classes = {1: "left", 2: "right"}
+
+data_path = ""
+train_test_ratio = 0.3
