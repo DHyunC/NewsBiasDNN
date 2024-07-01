130edfd5c52d2a32ae23009eddce76b83a4e3590
DHyunC <chsdaniel725@gmail.com>
2024-07-01 23:36:24 +1000

    initialised optimiser and criterion

--- train.py
+++ train.py
@@ -8,3 +8,8 @@ from params import *
 from dataset import *
 import torch
 import matplotlib.pyplot as plt
+
+optimiser = torch.optim.Adam(lr=learning_rate, betas=betas, 
+                             weight_decay=weight_decay, amsgrad=amsgrad)
+
+criterion = torch.nn.CrossEntropyLoss()
