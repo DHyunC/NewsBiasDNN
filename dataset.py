810ee33bac3b2e9b579643814f7fa4293da39633
DHyunC <chsdaniel725@gmail.com>
2024-07-06 16:32:10 +1000

    read files in data path and create corpus

--- dataset.py
+++ dataset.py
@@ -7,6 +7,7 @@ Author: Daniel H Choi
 from params import *
 from torch.utils.data import DataLoader, random_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+import os
 
 def generate_train_dataloader():
 
@@ -22,7 +23,17 @@ def generate_test_dataloader():
 
 # compute TF-IDF
 # merge documents into a single corpus
+train_data_path = "./train_data"
+
 corpus = []
+
+for file in os.listdir(train_data_path):
+    file_path = os.path.join(train_data_path, file)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            corpus.append(file.read())
+
 vectorizer = TfidfVectorizer()
 result = vectorizer.fit_transform(corpus)
 
